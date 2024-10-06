610acdbd8421923824d8ddbde2daa68b9098fa18
Cbarlop <cbarlop2506@gmail.com>
2024-10-07 00:14:51 +0200

    Adelantamiento de metodos en c#

--- Horas Libre Configuración/Tareas Entregar/EjerciciosC#/Ejercicio1/Ejercicio1/Ejercicio1/Vehiculo.cs
+++ Horas Libre Configuración/Tareas Entregar/EjerciciosC#/Ejercicio1/Ejercicio1/Ejercicio1/Vehiculo.cs
@@ -34,8 +34,8 @@ namespace Ejercicio1
 
         public marca MarcaCoche
         {
-             get { return marcaCoche; }
-             set { marcaCoche = value; }
+            get { return marcaCoche; }
+            set { marcaCoche = value; }
         }
 
         public tipo TipoCoche
@@ -72,11 +72,41 @@ namespace Ejercicio1
             set { numAirbags = value; }
         }
 
-        public Vehiculo()
+        //public Vehiculo(string matricula, string marcaCoche, string tipoCoche, int numPuerta, double precioBase, bool llantasAleacion, bool techoSolar, int numAirbags);
+        //{
+        //    this.matricula = matricula;
+        //    this.marcaCoche = marcaCoche;
+        //    this.tipoCoche = tipoCoche;
+        //    this.numPuerta = numPuerta;
+        //    this.precioBase = precioBase;
+        //    this.llantasAleacion = llantasAleacion;
+        //    this.techoSolar = techoSolar;
+        //    this.numAirbags = numAirbags;
+        //}
+
+        public double precioTecho(bool techoSolar)
         {
 
+            if (techoSolar == true)
+            {
+                this.precioBase = (this.precioBase * 1.023);
+            }
+            else
+            {
+                this.precioBase;
+            }
+
+            return this.precioBase;
+
         }
-        public Vehiculo(string matricula, string marcaCoche, string tipoCoche, int numPuerta, double pre, int numAirbags);
+
+        public double precioPuerta(int numPuerta)
+        {
+            if (numPuerta == 3)
+            {
+                this.precioBase = this.precioBase *
+            }
+        }
 
     }
 }
--- Horas Libre Configuración/Tareas Entregar/EjerciciosC#/Ejercicio1/Ejercicio1/Ejercicio1/obj/Release/net8.0/Ejercicio1.AssemblyInfo.cs
+++ Horas Libre Configuración/Tareas Entregar/EjerciciosC#/Ejercicio1/Ejercicio1/Ejercicio1/obj/Release/net8.0/Ejercicio1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41c4e2ce00e6b920b8bfff69cf88e1b819e85634")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercicio1")]
