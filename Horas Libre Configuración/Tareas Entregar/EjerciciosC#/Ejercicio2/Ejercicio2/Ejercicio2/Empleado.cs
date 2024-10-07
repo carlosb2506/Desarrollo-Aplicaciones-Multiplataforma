58a017c31740fa61e360b601bf2f8361364eb18f
Carlos <carlosbarroso2506@gmail.com>
2024-10-07 14:39:17 +0200

    Comienzo de Ejercicio 2

---
+++ Horas Libre Configuración/Tareas Entregar/EjerciciosC#/Ejercicio2/Ejercicio2/Ejercicio2/Empleado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    public class Empleado
+    {
+        private string nombre;
+        private int antiguedad;
+        public bool extranjero;
+        public int numHorasTrabajadas;
+        public enum niveles { JUNIOR , SENIOR , ANALISTA };
+
+        private niveles nivel;
+
+
+
+    }
+}
