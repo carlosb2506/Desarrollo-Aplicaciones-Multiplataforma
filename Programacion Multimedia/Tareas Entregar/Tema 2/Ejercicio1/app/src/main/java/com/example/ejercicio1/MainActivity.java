d90c920033f8535a28f4e0139a063b274fb0c44a
Cbarlop <cbarlop2506@gmail.com>
2024-10-11 10:40:29 +0200

    Ejercicio1 listo para entregar

--- Programacion Multimedia/Tareas Entregar/Tema 2/Ejercicio1/app/src/main/java/com/example/ejercicio1/MainActivity.java
+++ Programacion Multimedia/Tareas Entregar/Tema 2/Ejercicio1/app/src/main/java/com/example/ejercicio1/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.ejercicio1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioGroup rgOpc;
+
+    private TextView tvSalida;
+    private CheckBox cbAndorid, cbIphone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        rgOpc = findViewById(R.id.rgOpc);
+        tvSalida = findViewById(R.id.tvSalida);
+        cbAndorid = findViewById(R.id.cbAndroid);
+        cbIphone = findViewById(R.id.cbIphone);
+    }
+
+    public void mostrar(View view){
+
+        StringBuilder cadena = new StringBuilder();
+        RadioButton selectedRgOpc = findViewById(rgOpc.getCheckedRadioButtonId());
+
+        String selectedTextOpc = selectedRgOpc.getText().toString();
+
+        if (cbIphone.isChecked()) {
+            cadena.append(cbIphone.getText()).append("\n");
+        } if (cbAndorid.isChecked()){
+            cadena.append(cbAndorid.getText()).append("\n");
+        }
+        cadena.append(selectedTextOpc);
+        tvSalida.setText(cadena);
     }
 }
