09a2f50dafd4d43ee9de928fd6d406fdf0e627e3
Carlos <carlosbarroso2506@gmail.com>
2024-10-15 11:50:54 +0200

    Commit

--- Horas Libre Configuración/Tareas Entregar/EjerciciosC#/Ejercicio2/Ejercicio2/Ejercicio2/obj/Release/net8.0/Ejercicio2.AssemblyInfo.cs
+++ Horas Libre Configuración/Tareas Entregar/EjerciciosC#/Ejercicio2/Ejercicio2/Ejercicio2/obj/Release/net8.0/Ejercicio2.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+678b2c67de1fcb4d6cf2d035895323d189ff454b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercicio2")]
