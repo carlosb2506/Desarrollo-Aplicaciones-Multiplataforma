0fde12201de8e5f426781ec19909461cf4e303f2
Carlos <carlosbarroso2506@gmail.com>
2024-10-15 14:42:38 +0200

    Ejercicio 6 a falta de poder volver a jugar de nuevo

--- Programacion Multimedia/Tareas Entregar/Tema 2/Ejercicio6/app/src/main/java/com/example/ejercicio6/MainActivity.java
+++ Programacion Multimedia/Tareas Entregar/Tema 2/Ejercicio6/app/src/main/java/com/example/ejercicio6/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.ejercicio6;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,12 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     private ImageView ivTesoro;
     private Button btnConfirmar;
+    private TextView tvRestantes, tvPista;
+    private EditText tpNumMax, tpNumMin, tpNumAdv;
+    int intentosMax = 10;
+    boolean numadivinado;
+    int numAdivinar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +35,48 @@ public class MainActivity extends AppCompatActivity {
         });
 
         ivTesoro = findViewById(R.id.ivTesoro);
+        tvRestantes = findViewById(R.id.tvRestantes);
+        tpNumMax = findViewById(R.id.tpNumMax);
+        tpNumMin = findViewById(R.id.tpNumMin);
+        tpNumAdv = findViewById(R.id.tpNumAdv);
         btnConfirmar = findViewById(R.id.btnConfirmar);
+        tvPista = findViewById(R.id.tvPista);
+        numadivinado = false;
+
+
     }
 
     public void comprobar(View view){
-        ivTesoro.setVisibility(View.VISIBLE);
+        int numero = 0;
+        numAdivinar = Integer.parseInt(tpNumAdv.getText().toString());
+        if (numadivinado == false)
+        {
+            numero = (int) (Math.random() * Integer.parseInt(tpNumMax.getText().toString())) + Integer.parseInt(tpNumMin.getText().toString());
+            numadivinado = true;
+        }
+
+        if (intentosMax > 0)
+        {
+            if (numero != numAdivinar) {
+                if (numAdivinar < numero) {
+                    tvPista.setText("Pista: El numero a adivinar es menor");
+                } else {
+                    tvPista.setText("Pista: El numero a adivinar es mayor");
+                }
+            } else
+            {
+                ivTesoro.setVisibility(View.VISIBLE);
+            }
+            intentosMax --;
+            tvRestantes.setText("Intentos Restantes: " + intentosMax);
+        }else {
+            btnConfirmar.setEnabled(false);
+        }
+
+
+
+
+
 
     }
 }
