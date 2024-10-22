6437c95170f2207eabb8379231a441ccb6b85baf
Carlos <carlosbarroso2506@gmail.com>
2024-10-22 11:53:51 +0200

    Actualizacionn

---
+++ Horas Libre Configuración/Tareas Entregar/EjerciciosC#/Ejercicio3/Ejercicio3/Ejercicio3/Persona.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    public class Persona
+    {
+        private string nombre;
+        private string dni;
+        private char sexo;
+        private int edad;
+        private double peso;
+        private double altura;
+
+        public string Nombre{
+            get{ return nombre; } 
+            set{ nombre = value; }
+        }
+        public string Dni
+        {
+            get { return dni;  }
+            set { dni = value; }
+        }
+        public char Sexo
+        {
+            get { return sexo; }
+            set { sexo = value; }
+        }
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+        public double Peso
+        {
+            get { return peso; }
+            set { peso = value; }
+        }
+        public double Altura
+        {
+            get { return altura; }
+            set { altura = value; }
+        }
+
+
+        public Persona(string nombre, string dni, char sexo, int edad, double peso, double altura)
+        {
+            this.nombre = nombre;
+            this.dni = dni;
+            this.sexo = sexo;
+            this.edad = edad;
+            this.peso = peso;
+            this.altura = altura;
+        }
+        public Persona(string nombre, int edad, char sexo)
+        {
+            this.nombre = nombre;
+            this.dni = "";
+            this.peso = 0;
+            this.edad= edad;
+            this.sexo= sexo;
+            this.altura = 0;
+
+        }
+        public Persona()
+        {
+            this.nombre = "";
+            this.dni = "";
+            this.peso = 0;
+            this.edad = 0;
+            this.sexo = '';
+            this.altura = 0;
+        }
+    }
+
+    public int CalcularIMC(double peso, double altura)
+    {
+        int result = 0;
+        int calculo = (int) (peso / Math.Pow(altura, 2));
+        
+        if (calculo < 20)
+        {
+            result = -1;
+        }
+        else if (calculo >= 20 && calculo <= 25)
+        {
+            result = 0;
+        } else if (calculo > 25)
+        {
+            result = 1;
+        }
+
+        return result;
+    }
+
+    public bool MayorDeEdad(int edad)
+    {
+        bool mayor = false;
+
+        if (edad >= 18)
+        {
+            mayor = true;
+        }
+        else
+            mayor = false;
+
+        return mayor;
+    }
+
+    public void ComprobarSexo(char sexo)
+    {
+       
+        if (sexo.Equals('H') || sexo.Equals('h') || sexo.Equals('M') || sexo.Equals('m'))
+        {
+            Sexo = 
+        }
+    }
+}
--- Horas Libre Configuración/Tareas Entregar/EjerciciosC#/Ejercicio3/Ejercicio3/Ejercicio3/obj/Debug/net8.0/Ejercicio3.AssemblyInfo.cs
+++ Horas Libre Configuración/Tareas Entregar/EjerciciosC#/Ejercicio3/Ejercicio3/Ejercicio3/obj/Debug/net8.0/Ejercicio3.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+680d102a1d5b745d1877dd60a999f96150c0d110")]
-[assembly: System.Reflection.AssemblyProductAttribute("Ejercicio1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Ejercicio3")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
